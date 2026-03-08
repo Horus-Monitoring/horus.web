849d0a35d77729f37bb60bc136c0fdc90a2569ce
Matheus <matheusvalle06@gmail.com>
2026-03-07 23:20:45 -0300

    colocando nova versão script banco de dados

--- src/database/script.tabela.sql
+++ src/database/script.tabela.sql
@@ -2,13 +2,6 @@ CREATE DATABASE horus_db;
 
 USE horus_db;
 
-CREATE TABLE Papel (
-idPapel INT PRIMARY KEY AUTO_INCREMENT,
-nivel VARCHAR(45),
-descricao VARCHAR(80)
-);
-
-
 CREATE TABLE Localizacao (
 idLocalizacao INT PRIMARY KEY AUTO_INCREMENT,
 uf CHAR(2),
@@ -18,6 +11,7 @@ logradouro VARCHAR(45),
 numero INT,
 cep CHAR(11)
 );
+
 
 
 CREATE TABLE Empresa (
@@ -34,80 +28,107 @@ CONSTRAINT fk_localizacao_registro
 
 
 
+
+CREATE TABLE Papel (
+idPapel INT  AUTO_INCREMENT,
+nivel VARCHAR(45),
+descricao VARCHAR(80),
+fk_empresa INT, 
+CONSTRAINT pk_papel_empresa
+	PRIMARY KEY(idPapel, fk_empresa),
+CONSTRAINT fk_empresa_registro
+	FOREIGN KEY (fk_empresa)
+		REFERENCES Empresa (idEMpresa)
+);
+
+
+
+
+
+
 CREATE TABLE Funcionario (
 idFuncionario INT AUTO_INCREMENT,
-fk_empresa INT,
 nome VARCHAR(45),
+nome_social VARCHAR(45),
 cpf CHAR(11) NOT NULL UNIQUE,
 email VARCHAR(45) NOT NULL UNIQUE,
 senha VARCHAR(45),
-fk_papel INT,
-CONSTRAINT pk_funcionario_empresa
-	PRIMARY KEY(idFuncionario, fk_empresa),
-CONSTRAINT fk_empresa_registro
-	FOREIGN KEY (fk_empresa)
-		REFERENCES Empresa(idEmpresa),
+fk_papel_empresa INT,
+CONSTRAINT pk_funcionario_papel_empresa
+	PRIMARY KEY(idFuncionario, fk_papel_empresa),
 CONSTRAINT fk_papel_registro
-	FOREIGN KEY (fk_papel)
+	FOREIGN KEY (fk_papel_empresa)
 		REFERENCES Papel(idPapel)
 );
 
 
-CREATE TABLE Datacenter (
-idDatacenter INT AUTO_INCREMENT,
-fk_empresa INT,
-qtd_comp INT,
-data_instalacao DATE,
-fk_localizacao INT,
-CONSTRAINT pk_datacenter_empresa
-	PRIMARY KEY(idDatacenter, fk_empresa),
-CONSTRAINT fk_empresa_registro_datacenter
-	FOREIGN KEY (fk_empresa)
-		REFERENCES Empresa(idEmpresa),
-CONSTRAINT fk_localizacao_registro_datacenter
-	FOREIGN KEY (fk_localizacao)
-		REFERENCES Localizacao (idLocalizacao)
- );
+
+
+
+
  
  
  CREATE TABLE Servidor (
- idServidor INT AUTO_INCREMENT,
- fk_datacenter INT,
- so VARCHAR(45),
- modelo_servidor VARCHAR(45),
- qtd_ram INT, 
- qtd_disc INT,
- modelo_cpu VARCHAR(45),
- CONSTRAINT pk_servidor_datacenter
-	PRIMARY KEY (idServidor, fk_datacenter),
- CONSTRAINT fk_datacenter_registro
-	FOREIGN KEY (fk_datacenter)
-		REFERENCES Datacenter(idDatacenter)
+idServidor INT AUTO_INCREMENT,
+data_instalacao DATE,
+tag_servidor VARCHAR(45),
+fk_empresa INT,
+ CONSTRAINT pk_servidor_empresa
+	PRIMARY KEY (idServidor, fk_empresa),
+CONSTRAINT fk_empresa_registro_servidor
+	FOREIGN KEY (fk_empresa)
+		REFERENCES Empresa(idEmpresa)
 );
 
-CREATE TABLE Log_medicao (
-idMedicao INT AUTO_INCREMENT,
-fk_servidor INT, 
-cpu_atual DECIMAL(4,2) NOT NULL,
-ram_atual DECIMAL(4,2) NOT NULL,
-disco_atual DECIMAL(4,2) NOT NULL,
-time_log DATETIME DEFAULT CURRENT_TIMESTAMP, 
-CONSTRAINT pk_servidor_medicao
-	PRIMARY KEY (idMedicao, fk_servidor),
+CREATE TABLE Componentes (
+idComponentes INT PRIMARY KEY AUTO_INCREMENT,
+nome_componente VARCHAR(45),
+tipo_componente VARCHAR(45),
+unidade_medida VARCHAR(45)
+);
+
+
+CREATE TABLE CompServidor (
+id_componente_v INT AUTO_INCREMENT,
+fk_componente INT,
+fk_servidor INT,
+limite VARCHAR(45),
+CONSTRAINT pk_componente_servidor_componente_v
+	PRIMARY KEY (id_componente_v, fk_componente, fk_servidor),
+CONSTRAINT fk_componente_registro
+	FOREIGN KEY (fk_componente)
+		REFERENCES Componentes(idComponentes),
 CONSTRAINT fk_servidor_registro
 	FOREIGN KEY (fk_servidor)
-		REFERENCES Servidor (idServidor)
+		REFERENCES Servidor(idServidor)
 );
 
 
-CREATE TABLE Alerta (
-idAlerta INT AUTO_INCREMENT,
-fk_logmedicao INT, 
+CREATE TABLE Registro_Alerta (
+idAlerta INT PRIMARY KEY AUTO_INCREMENT,
 data_alerta DATETIME DEFAULT CURRENT_TIMESTAMP, 
 criticidade VARCHAR(45), 
-CONSTRAINT pk_alerta_logmedicao
-	PRIMARY KEY (idAlerta, fk_logmedicao),
-CONSTRAINT fk_logmedicao_registro
-	FOREIGN KEY (fk_logmedicao)
-		REFERENCES Log_medicao (idMedicao)
+fk_servidor_componentes INT,
+CONSTRAINT fk_servidor_componente
+	FOREIGN KEY (fk_servidor_componentes)
+		REFERENCES CompServidor(id_componente_v)
 ); 
+
+
+INSERT INTO Localizacao (uf, cidade, bairro, logradouro, numero, cep) VALUES
+	('SP', 'São Paulo', 'Paulista','Rua Hadock Lobo', 12, '08412210');
+    
+INSERT INTO Empresa (razao_social, cnpj, telefone_empresa, token_empresa, fk_localizacao) VALUES
+	('LIPSU AERO', '123456789101234', '5573-1234', 'ABC12345', 1);
+    
+INSERT INTO Papel (nivel, descricao, fk_empresa) VALUES
+	('Gerente de ATC', 'Deve monitorar e solucionar problemas', 1);
+    
+INSERT INTO Funcionario (fk_papel_empresa, nome,  cpf, email, senha) VALUES
+	(1, 'Herycka', '32187634567', 'herycka@gmail.com', 'Herycka_1234');
+    
+    
+SELECT  nivel  FROM Papel JOIN Funcionario
+	ON idPapel = fk_papel_empresa
+    WHERE nome = 'Herycka';
+    
