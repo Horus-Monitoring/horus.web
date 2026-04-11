150d4e5f49fc3782e643f4cc28fb5ef8608b0aca
mtrbz <matheus.rbarros@sptech.school>
2026-04-10 23:59:49 -0300

    cadastro de componente funcionando e listagem de servidores cadastrados

--- src/controllers/servidoresController.js
+++ src/controllers/servidoresController.js
@@ -1,5 +1,6 @@
 var servidoresModel = require("../models/servidoresModel");
 
+// Servidores
 function cadastrarServidor(req, res) {
     var fkEmpresa = req.body.fkEmpresaServer
     var nome = req.body.nomeServer;
@@ -41,6 +42,87 @@ function cadastrarServidor(req, res) {
     }
 }
 
+function exibirServidores(req, res) {
+    var fkEmpresa = req.params.fkEmpresa
+    
+    servidoresModel.exibirServidores(fkEmpresa)
+        .then(
+            (resultado => {
+                res.json(resultado);
+            })
+        ).catch(
+            (erro => {
+                console.log(erro);
+                console.log(
+                    "\nHouve um erro ao exibir os servidores!\nErro: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }) 
+        );
+}
+
+function listarServidores(req, res) {
+    var fkEmpresa = req.params.fkEmpresa
+    
+    servidoresModel.listarServidores(fkEmpresa)
+        .then(
+            (resultado => {
+                res.json(resultado);
+            })
+        ).catch(
+            (erro => {
+                console.log(erro);
+                console.log(
+                    "\nHouve um erro ao listar os servidores!\nErro: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }) 
+        );
+}
+
+// Componentes
+function cadastrarComponente(req, res) {
+    var fkServidor = req.body.fkServidorServer
+    var fkComponente = req.body.fkComponenteServer;
+    var marca = req.body.marcaServer;
+    var modelo = req.body.modeloServer;
+    var statusInicial = req.body.statusInicialServer;
+
+    if (fkServidor == undefined) {
+        res.status(400).send("fkServidor está undefined!");
+    } else if (fkComponente == undefined) {
+        res.status(400).send("fkComponente está undefined!");
+    } else if (marca == undefined) {
+        res.status(400).send("marca está undefined!");
+    } else if (modelo == undefined) {
+        res.status(400).send("modelo está undefined!");
+    } else if (statusInicial == undefined) {
+        res.status(400).send("statusInicial está undefined!");
+    } else {
+        
+        servidoresModel.cadastrarComponente(fkServidor, fkComponente, marca, modelo, statusInicial)
+            .then(
+                (resultado => {
+                    res.json(resultado);
+                })
+            ).catch(
+                (erro => {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao cadastrar o servidor!\nErro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }) 
+            );
+    }
+}
+
 module.exports = {
-    cadastrarServidor
+    cadastrarServidor,
+    exibirServidores,
+    listarServidores,
+    cadastrarComponente
 }
