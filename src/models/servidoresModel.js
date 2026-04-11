5fcd8512039e04ebf6c734cae726e35a6e76383e
mtrbz <matheus.rbarros@sptech.school>
2026-04-10 23:59:49 -0300

    cadastro de componente funcionando e listagem de servidores cadastrados

--- src/models/servidoresModel.js
+++ src/models/servidoresModel.js
@@ -1,5 +1,6 @@
 var database = require("../database/config")
 
+// Servidores
 function cadastrarServidor(nome, ip, localizacao, sistemaOperacional, statusInicial, fkEmpresa) {
     console.log("ACESSEI O MODEL SERVIDORES \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, ip, localizacao, sistemaOperacional, statusInicial, fkEmpresa);
 
@@ -10,6 +11,40 @@ function cadastrarServidor(nome, ip, localizacao, sistemaOperacional, statusInic
     return database.executar(instrucaoSql);
 }
 
+function exibirServidores(fkEmpresa) {
+    console.log("ACESSEI O MODEL SERVIDORES \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", fkEmpresa);
+
+    var instrucaoSql = `
+        SELECT * FROM servidor WHERE fk_empresa = ${fkEmpresa};
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function listarServidores(fkEmpresa) {
+    console.log("ACESSEI O MODEL SERVIDORES \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", fkEmpresa);
+
+    var instrucaoSql = `
+        SELECT id_servidor, nome FROM servidor WHERE fk_empresa = ${fkEmpresa};
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+// Componentes
+function cadastrarComponente(fkServidor, fkComponente, marca, modelo, statusInicial) {
+    console.log("ACESSEI O MODEL SERVIDORES \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", fkServidor, fkComponente, marca, modelo, statusInicial);
+
+    var instrucaoSql = `
+        INSERT INTO servidor_componente (fk_servidor, fk_componente, marca, modelo, status_inicial) VALUES ('${fkServidor}', '${fkComponente}', '${marca}', '${modelo}', '${statusInicial}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
-    cadastrarServidor
+    cadastrarServidor,
+    exibirServidores,
+    listarServidores,
+    cadastrarComponente
 };
--- src/routes/servidores.js
+++ src/routes/servidores.js
@@ -3,8 +3,22 @@ var router = express.Router();
 
 var servidoresController = require("../controllers/servidoresController");
 
+// Servidoes
 router.post("/cadastrarServidor", function (req, res) {
     servidoresController.cadastrarServidor(req, res);
+})
+
+router.get("/exibirServidores/:fkEmpresa", function (req, res) {
+    servidoresController.exibirServidores(req, res);
+})
+
+router.get("/listarServidores/:fkEmpresa", function (req, res) {
+    servidoresController.listarServidores(req, res);
+})
+
+// Componentes
+router.post("/cadastrarComponente", function (req, res) {
+    servidoresController.cadastrarComponente(req, res);
 })
 
 module.exports = router;
