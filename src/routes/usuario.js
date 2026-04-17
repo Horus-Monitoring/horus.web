40c834f90aed48235474e5261d427c9a7455ca03
Gustavo Pietro <gusoares.ma@gmail.com>
2026-04-16 21:52:09 -0300

    Desenvolvendo rotas para deletar usuario

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -12,4 +12,8 @@ router.post("/autenticar", function (req, res) {
     usuarioController.autenticar(req, res);
 });
 
+router.delete("/deletarUsuario:id", function (req, res) {
+    usuarioController.deletarUsuario(req, res);
+});
+
 module.exports = router;
