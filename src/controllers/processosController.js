d9780fad2ca37b6096f820e434126f53102cae4c
letscostz <leticia.nascimento@sptech.school>
2026-05-19 18:25:57 -0300

    conexao com s3, routes e controllers

---
+++ src/controllers/processosController.js
@@ -0,0 +1,49 @@
+// npm i @aws-sdk/client-s3 (conexao entre s3 e front)
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+
+const s3 = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        sessionToken: process.env.AWS_SESSION_TOKEN
+    }
+});
+
+async function capturarDados(req, res) {
+
+    try {
+
+        const command = new GetObjectCommand({
+
+            Bucket: "horus-monitoring",
+
+            // client -> id da empresa -> mac address -> processos.json 
+            Key:
+                `client/empresa_1/c0:35:32:c7:0b:59/processos.json`
+        });
+
+        // aguardando resposta da requisição
+        const data = await s3.send(command);
+
+        // transforma json em string
+        const jsonString =
+            await data.Body.transformToString();
+
+        const json = JSON.parse(jsonString);
+
+        res.json(json);
+
+    } catch (erro) {
+
+        console.log(erro);
+
+        res.status(500).json({
+            erro: "Erro ao buscar dashboard"
+        });
+    }
+}
+
+module.exports = {
+    capturarDados
+}
