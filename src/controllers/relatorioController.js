ff8e19e828bbc64e99e3703cfd730371e0d7c243
jortieke <joao.jortiekejr@sptech.school>
2026-05-20 21:54:28 -0300

    Adaptação da rota para gerar o relatório após inserção do mac_address

--- src/controllers/relatorioController.js
+++ src/controllers/relatorioController.js
@@ -3,53 +3,23 @@ const { spawn } = require("child_process");
 
 async function gerarRelatorio(req, res){
 
-    const { servidor, email } = req.params;
+    const { usuario, email, mac_address, servidor, id_empresa } = req.params;
     
 
     const caminhoJar = path.resolve(
         "C:/Users/ricar/Documents/SP Tech/CCO/2 Semestre/Projeto/Backend/JarExecutavel/target/JarExecutavel-1.0-SNAPSHOT.jar"
-    );
+    ); //Deixar dinâmico após subir no docker da EC2 FAZER!!
     let caminhoPDF = "";
 
     const javaProcess = spawn("java", [
         "-jar",
-        "relatorio.jar",
+        "relatorio.jar", //Mudar?  FAZER!!
+        usuario,
         email,
-        servidor
+        mac_address,
+        servidor,
+        id_empresa
     ]);
-
-    javaProcess.stdout.on("data", (data)=>{
-        caminhoPDF += data.toString();
-    });
-
-    javaProcess.on("close", ()=>{
-
-        caminhoPDF = caminhoPDF.trim();
-
-        res.download(caminhoPDF);
-    });
-
-}
-
-async function gerarRelatorio(req, res){
-
-    let { servidor, email } = req.params;
-
-    servidor = decodeURIComponent(servidor);
-    email = decodeURIComponent(email);
-
-    const caminhoJar = path.resolve(
-        "C:/Users/ricar/Documents/SP Tech/CCO/2 Semestre/Projeto/Backend/JarExecutavel/target/JarExecutavel-1.0-SNAPSHOT.jar"
-    );
-
-    const javaProcess = spawn("java", [
-        "-jar",
-        caminhoJar,
-        email,
-        servidor
-    ]);
-
-    let caminhoPDF = "";
 
     javaProcess.stdout.on("data", (data)=>{
         caminhoPDF += data.toString();
